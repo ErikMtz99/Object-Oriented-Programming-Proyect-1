d864081df05ab46458af80155788bfe480b886ee
ErikMtz99 <61127579+ErikMtz99@users.noreply.github.com>
2020-02-26 18:54:53 -0600

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,4 @@
-// Proyecto #1 Erik Fernando Martínez Martínez A01244818
+// Proyecto #1 Erik Fernando MartÃ­nez MartÃ­nez A01244818
 
 #include <iostream>
 #include <fstream>
@@ -14,25 +14,17 @@ int main() {
 
 	cout << "Ingrese nombre del archivo de entrada: " << endl;
 	cin >> nombre_entrada;
-	cout << "Ingrese nombre del archivo de salida (Poner .txt al final porfavor) : " << endl;
+	cout << "Ingrese nombre del archivo de salida: " << endl;
 	cin >> nombre_salida;
-
-	archivo_sal.open(nombre_salida);
 
 	if (nombre_entrada == "Operaciones.txt") {
 		archivo_ent.open("Operaciones.txt");
-
-		string x;
-		while (getline(archivo_ent,x)) {
-			cout << x << endl;
-		}
 	}
 	else {
-		cout << "No existe el archivo de entrada." << endl;
+		cout << "No existe el archivo." << endl;
 	}
 
-	cout << "Se ha creado el archivo con nombre: " << nombre_salida << endl;
-	archivo_sal.close();
+	archivo_sal.open(nombre_salida);
 	archivo_ent.close();
 	return 0;
 }
